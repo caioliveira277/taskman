c241ba038f96c003d0eaac934fa5b191cb5efa01
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-04-21 22:35:27 -0300

    finished login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Main, Section, Figure } from "./styles";
+import Form from "../../components/forms";
+import Modal from "../../components/modal";
 
 export default function Login() {
-  return <h1>login</h1>;
+  const changeForm = useSelector((state) => state.ChangeForm.form);
+  const toggleModal = useSelector((state) => state.ToggleModal.modal);
+  const [mode, setMode] = useState(null);
+  const [renderModal, setRenderModal] = useState(null);
+
+  useEffect(() => {
+    setMode(changeForm);
+  }, [changeForm]);
+
+  useEffect(() => {
+    if (toggleModal === "Open") {
+      setRenderModal(<Modal />);
+    } else {
+      setRenderModal(null);
+    }
+  }, [toggleModal]);
+
+  return (
+    <Main>
+      <Section>
+        <Figure mode={mode}>
+          <img
+            src={`${process.env.PUBLIC_URL}/assets/images/login.png`}
+            alt="Task organization"
+          />
+        </Figure>
+        <Form />
+      </Section>
+      {renderModal}
+    </Main>
+  );
 }
