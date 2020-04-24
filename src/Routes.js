39c9245cb59341258563cfd2214f1f19fde1fd3a
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-04-23 21:27:34 -0300

    add profile and private routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,48 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import { Provider } from "react-redux";
-import Login from "./pages/login";
+import PropTypes from "prop-types";
 import Store from "./store";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
+
+const isAuthenticated = () => true;
+
+const PrivateRoute = ({ component: Component }) => {
+  const PrivateFn = () => {
+    return (
+      <Route
+        render={({ history, location, match }) => {
+          if (isAuthenticated())
+            return (
+              <Component history={history} location={location} match={match} />
+            );
+
+          return <Redirect to={{ pathname: "/", state: { from: location } }} />;
+        }}
+      />
+    );
+  };
+  return PrivateFn();
+};
+PrivateRoute.prototype = {
+  component: PropTypes.func.isRequired,
+  location: PropTypes.string,
+};
 
 export default function Routes() {
   return (
     <Provider store={Store}>
       <Router>
-        <Route exact path="/login" component={Login} />
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <PrivateRoute exact path="/profile" component={Profile} />
+        </Switch>
       </Router>
     </Provider>
   );
