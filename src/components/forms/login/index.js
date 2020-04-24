f087de2e8537ec9d08efbac77abf977f4c180a9e
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-04-23 21:28:54 -0300

    add transition

--- src/components/forms/login/index.js
+++ src/components/forms/login/index.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdVpnKey, MdPerson } from "react-icons/md";
 import { useDispatch } from "react-redux";
 import Form from "./styles";
 import SwitchInput from "../../switch";
 import ButtonTheme from "../../buttons";
+import Transition from "../../transition";
 
 export default function LoginForm() {
   const FormDispatch = useDispatch();
   const ModalDispatch = useDispatch();
+  const [transition, setTrantision] = useState(null);
 
   const ChangeForm = (event) => {
     event.preventDefault();
@@ -18,34 +20,43 @@ export default function LoginForm() {
     event.preventDefault();
     ModalDispatch({ type: "OPEN" });
   };
+
+  const HandleSubmit = (event) => {
+    event.preventDefault();
+    setTrantision(<Transition redirect="/profile" />);
+  };
+
   return (
-    <Form>
-      <em>
-        <p>Acesso ao sistema</p>
-        <a href="/" onClick={ChangeForm}>
-          Não tem cadastro?
-        </a>
-      </em>
-      <h1>Log in</h1>
-      <div>
-        <MdPerson />
-        <input
-          type="text"
-          autoComplete="username"
-          placeholder="Nome de usuário"
-        />
-      </div>
-      <div>
-        <MdVpnKey />
-        <input type="password" placeholder="Senha" />
-      </div>
-      <span>
-        <SwitchInput text="Lembrar?" color="var(--color-orange)" />
-        <a href="/" onClick={ToggleModal}>
-          Esqueceu sua senha?
-        </a>
-      </span>
-      <ButtonTheme type="submit" text="Entrar" />
-    </Form>
+    <>
+      <Form onSubmit={HandleSubmit}>
+        <em>
+          <p>Acesso ao sistema</p>
+          <a href="/" onClick={ChangeForm}>
+            Não tem cadastro?
+          </a>
+        </em>
+        <h1>Log in</h1>
+        <div>
+          <MdPerson />
+          <input
+            type="text"
+            autoComplete="username"
+            placeholder="Nome de usuário"
+          />
+        </div>
+        <div>
+          <MdVpnKey />
+          <input type="password" placeholder="Senha" />
+        </div>
+        <span>
+          <SwitchInput text="Lembrar?" color="var(--color-orange)" />
+          <a href="/" onClick={ToggleModal}>
+            Esqueceu sua senha?
+          </a>
+        </span>
+        <ButtonTheme type="submit" text="Entrar" />
+      </Form>
+      {transition}
+    </>
   );
 }
---
+++ src/pages/profile/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Main } from "./styles";
+import Transition from "../../components/transition";
+
+export default function Profile() {
+  return (
+    <>
+      <Main>
+        <h1>Profile</h1>
+      </Main>
+      <Transition mode="reverse" />
+    </>
+  );
+}
