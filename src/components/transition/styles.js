f4d085b1cbe74a0c933ffb1e127f39763c9c0872
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-04-25 13:08:12 -0300

    change the transition animation

--- src/components/transition/styles.js
+++ src/components/transition/styles.js
@@ -1,19 +1,33 @@
 import styled from "styled-components";
-import { TransitionAnim, TransitionAnimReverse } from "../animations";
+import {
+  TransitionAnim,
+  TransitionAnimReverse,
+  FadeTextTransition,
+} from "../animations";
 
 const Transition = styled.section`
   background: var(--color-blue);
   position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   animation: ${(props) =>
       props.mode === "reverse" ? TransitionAnimReverse : TransitionAnim}
-    1.2s ease-in-out
-    ${(props) => (props.mode === "reverse" ? "forwards" : "forwards")};
+    600ms ease ${(props) => props.mode} forwards;
   will-change: transform;
   z-index: 5;
   top: 0;
   left: 0;
   height: 100vh;
+  width: 100vw;
   ${(props) => (props.mode === "reverse" ? "width: 100vw;" : "")};
+  h1 {
+    font: normal 3.5rem monospace;
+    color: #ffffff;
+    opacity: 0;
+    transform: translateY(50%);
+    animation: ${FadeTextTransition} 800ms 700ms alternate;
+  }
 `;
 
 export default Transition;
