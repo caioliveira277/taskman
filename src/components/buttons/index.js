52c54d103882cdf5ba88e408721137230b5392a3
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-04-25 13:13:45 -0300

    add comment function

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -2,13 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import Button from "./styles";
 
+/**
+ * @description Button Component
+ *
+ * @param   {String} [required]    type
+ * @param   {String} [required]    text
+ * @return {JSX} JSX
+ *
+ * @example
+ *   <Button type="submit" text="Send"/>
+ */
 export default function ButtonTheme({ type, text }) {
-  const ButtonFn = () => {
-    return <Button type={type}>{text}</Button>;
-  };
-  return ButtonFn();
+  return <Button type={type}>{text}</Button>;
 }
-ButtonTheme.prototype = {
+ButtonTheme.propTypes = {
+  type: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
-  type: PropTypes.string,
 };
