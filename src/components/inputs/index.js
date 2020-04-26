5958f71ed1552b8dd21e99541c300e520ebd8c33
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-04-26 15:39:41 -0300

    added news components

--- src/components/inputs/index.js
+++ src/components/inputs/index.js
@@ -1,4 +1,6 @@
 import Input from "./input";
 import Switch from "./switch";
+import ColorPicker from "./colorPicker";
+import TextArea from "./textarea";
 
-export { Input, Switch };
+export { Input, Switch, ColorPicker, TextArea };
