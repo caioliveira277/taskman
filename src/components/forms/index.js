59cdfca4d6c2b4456d3e0d7b61a10279b34e2238
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-05-02 17:22:20 -0300

    add new form component

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -2,8 +2,11 @@ import React from "react";
 import { useSelector } from "react-redux";
 import FormLogin from "./login";
 import FormSignIn from "./signIn";
+import NewGroup from "./newGroup";
 
 export function Form() {
   const changeForm = useSelector((state) => state.ChangeForm.form);
   return changeForm === "Login" ? <FormLogin /> : <FormSignIn />;
 }
+
+export { NewGroup };
---
+++ src/components/forms/newGroup/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { RiMenuAddLine } from "react-icons/ri";
+import Group from "./styles";
+
+export default function GroupForm() {
+  return (
+    <Group>
+      <span>
+        <i>
+          <RiMenuAddLine />
+        </i>
+      </span>
+    </Group>
+  );
+}
