ba89e823e9a7d718f528cb8cbf570048da823171
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-05-02 17:17:06 -0300

    added task component

--- src/pages/tasks/index.js
+++ src/pages/tasks/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Main } from "./styles";
+import { NewGroup } from "../../components/forms";
+import Group from "../../components/group";
+import Task from "../../components/task";
 import Transition from "../../components/transition";
 import Header from "../../components/header";
 import Menu from "../../components/menu";
@@ -10,8 +13,54 @@ export default function Tasks() {
       <Main>
         <Header />
         <section>
-          <Menu />
+          <Group title="Group 1">
+            <>
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+            </>
+          </Group>
+          <Group title="Group 2" backgroundColor="#f44336">
+            <>
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+            </>
+          </Group>
+          <Group title="Group 3" backgroundColor="#8bc34a">
+            <>
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+              <Task />
+            </>
+          </Group>
+          <NewGroup />
         </section>
+        <Menu />
       </Main>
       <Transition mode="reverse" />
     </>
--- src/pages/tasks/styles.js
+++ src/pages/tasks/styles.js
@@ -3,5 +3,13 @@ import styled from "styled-components";
 export const Main = styled.main`
   width: 100vw;
   height: 100vh;
-  background: url("https://images.wallpaperscraft.com/image/books_inscription_bw_168086_1920x1080.jpg");
+  background: url("https://images.wallpaperscraft.com/image/fern_leaf_plant_green_119970_1920x1080.jpg");
+  overflow-x: hidden;
+  section {
+    margin: 1.5rem;
+    justify-content: space-around;
+    grid-gap: 1.5rem;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 300px);
+  }
 `;
